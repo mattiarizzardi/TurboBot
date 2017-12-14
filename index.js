3ee903f4a6a960eb679ab60aca54b5d063458039
antoniomasciaa <antonio.mascia@studenti.unitn.it>
2017-12-14 13:15:21 +0100

    fixed random session

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const env = require('./env.js');
 const APIAI_TOKEN = env.APIAI_TOKEN;
 const apiai = require('apiai')(APIAI_TOKEN);
+const ArrayList = require('ArrayList');
 
 app.use('/', express.static(__dirname + '/views')); // html
 app.use(express.static(__dirname + '/public')); // js, css, images
@@ -14,18 +15,19 @@ const server = app.listen(process.env.PORT || 5000, function () {
 });
 
 const io = require('socket.io')(server);
+var sessionList = new ArrayList;
 io.on('connection', function (socket) {
     //console.log('a user connected');
-    var session = Math.floor((Math.random() * 5000) + 1); // Create random number used for unique session
-    socket.on('chat message', function (text) {
+    var sessionId =uniqueSession(sessionList)
 
+    socket.on('chat message', function (text) {
 
         // Get a reply from API.ai
 
         var apiaiReq = apiai.textRequest(text, {
-            sessionId: session
+            sessionId: sessionId
         });
-
+        //console.log(sessionId);
         apiaiReq.on('response', function (response) {
             var aiText = response.result.fulfillment.messages[0].speech;
             aiText = aiText.replace(/\n/g,'\<br>') // new line in html
@@ -40,3 +42,13 @@ io.on('connection', function (socket) {
 
     });
 });
+
+function uniqueSession(sessionList){
+    var session = Math.floor((Math.random() * 5000) + 1); // Create random number used for unique session
+    while (sessionList.contains(session)) {
+        session = Math.floor((Math.random() * 5000) + 1);
+        sessionList.set(sessionList.size(), session);
+    }
+    console.log(session)
+    return session;
+}
