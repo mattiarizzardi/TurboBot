a2175c751daacd352ede4ed1ea127863e09a0e48
Mattia <mattia.pavan@studenti.unitn.it>
2017-12-20 10:51:00 +0100

    Staticwebpage (#12)
    * Improve static page
    
    * modified script
    
    * Fixing CSS
    
    Fixed send button alignment
    
    * Add initial message animation
    
    * Minor static changes
    
    Fixing header image style to match with the rest of the page, minor language fix on send button
    
    * Added dynamic scrollbar and fixed bottom chatbox layout
    
    Added dynamic scrollbar to chatbox and fixed layout (going under input bar)
    
    * Added autoscroll
    
    Automatic autoscroll when new content is added now implemented
    
    * commit error
    
    commit error
    
    * Revert "commit error"
    
    This reverts commit 84beb1b354fb79a8ae7ae0769b078e80bfddaf1a.

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,23 @@
 
 const socket = io();
 var d = new Date();
-var h = d.getHours();
-var m = d.getMinutes();
+jQuery(window).load(function () {
+    var h = d.getHours();
+    var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+    setTimeout(function () {
+        $('#chatbox').append(' <div class="container darker">\n' +
+            '        <img src="/img/avatar_bot.png" alt="Bot" class="right" style="width:100%;">\n' +
+            '        <p>' + 'Ciao, come posso aiutarti?' + ' </p>\n' +
+            '        <span class="time-right">' + h + ':' + m + '  </span>\n' +
+            '    </div>'
+        );
+    }, 1500);
+
+});
 $('form').submit(function () {
     socket.emit('chat message', $('#m').val());
-    h = d.getHours();
-    m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+    var h = d.getHours();
+    var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
     $('#chatbox').append(' <div class="container">\n' +
         '        <img src="/img/avatar_user.jpg" alt="Tu">\n' +
         '        <p>' + $('#m').val() + '</p>\n' +
@@ -15,18 +26,24 @@ $('form').submit(function () {
 
         '    </div>'
     );
+$('#chatbox').animate({"scrollTop": $('#chatbox')[0].scrollHeight}, "fast");
+
     $('#m').val('');
     return false;
 });
 
 socket.on('bot reply', function (replyText) {
     if (replyText == '') replyText = '(No answer...)';
-    h = d.getHours();
-    m = (d.getMinutes()<10?'0':'') + d.getMinutes();
+    var h = d.getHours();
+    var m = (d.getMinutes()<10?'0':'') + d.getMinutes();
     $('#chatbox').append(' <div class="container darker">\n' +
         '        <img src="/img/avatar_bot.png" alt="Bot" class="right" style="width:100%;">\n' +
         '        <p>' + replyText + ' </p>\n' +
         '        <span class="time-right">' + h + ':' + m + '  </span>\n' +
         '    </div>'
+
     );
-});
+$('#chatbox').animate({"scrollTop": $('#chatbox')[0].scrollHeight}, "fast");
+}
+
+);
